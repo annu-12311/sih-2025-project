851f71f1cabd12c9973c2f19eab2055f5154432b
hariomdhakad <hariomdhakad16122002@gmail.com>
2025-12-16 16:08:11 +0530

    rename Header and Footer files, update Header links, and add Footer Storybook configuration

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -3,7 +3,7 @@ export const Header = () => {
         <header style={{ background: '#1976d2', color: '#fff', padding: '1rem' }}>
             <h1 style={{ margin: 0 }}>Digital Learning App</h1>
             <nav>
-                <a href="/" style={{ color: '#fff', marginRight: '1rem' }}>Home</a>
+                <a href="/public" style={{ color: '#fff', marginRight: '1rem' }}>Home</a>
                 <a href="/lessons" style={{ color: '#fff', marginRight: '1rem' }}>Lessons</a>
                 <a href="/dashboard" style={{ color: '#fff' }}>Dashboard</a>
             </nav>
